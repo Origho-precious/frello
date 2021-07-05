1238f7bc054591c4dd9522ec91d818726dd7c171
Origho-precious <origho9@gmail.com>
2021-07-05 23:16:43 +0100

    feat: adding Input component

---
+++ frontend/src/components/atoms/Input/Input.tsx
@@ -0,0 +1,38 @@
+import styled from "styled-components";
+
+interface IInputProps {
+	disabled?: boolean;
+	type: string;
+	value: string;
+	onChange: (e: any) => void;
+	name: string;
+	placeholder?: string;
+	style?: React.CSSProperties;
+	className?: string;
+}
+
+const Input: React.FC<IInputProps> = ({ disabled, className, ...rest }) => {
+	return (
+		<Element
+			disabled={disabled}
+			{...rest}
+			className={`${className ? className : ""} text-sm block w-full`}
+		/>
+	);
+};
+
+const Element = styled.input`
+	border-bottom: 2px solid #99b9ee;
+	outline: none;
+	padding: 0.3rem 0.5rem;
+
+	&:hover {
+		border-bottom: 2px solid #99b9ee;
+	}
+
+	&:focus {
+		background: #F7F7F7;
+	}
+`;
+
+export default Input;
