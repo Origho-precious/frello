ecf0c2f9c7a2c13033b6f67dfccd87bcdf206ef6
Origho-precious <origho9@gmail.com>
2021-07-09 05:08:42 +0100

    feat: adding login endpoint

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -11,11 +11,30 @@ export const createUser = asyncHandler(async (req, res, next) => {
 		throw new Error("Some required field(s) not found!");
 	} else if (userExists) {
 		res.status(401);
-		throw new Error(`User with Email already exists`);
+		throw new Error(`User account associated with ${email} already exists`);
 	} else {
 		const user = await User.create({ name, email, password });
+		res.status(201).json({
+			_id: user._id,
+			name: user.name,
+			email: user.email,
+			token: generateToken(user._id),
+		});
+	}
+});
 
-		res.json({
+export const authenticateUser = asyncHandler(async (req, res, next) => {
+	const { email, password } = req.body;
+	const user = await User.findOne({ email });
+
+	if (!email || !password) {
+		res.status(400);
+		throw new Error("Required field(s) not found!");
+	} else if (!user) {
+		res.status(401);
+		throw new Error(`User account associated with ${email} doesn't exists`);
+	} else if (user && user.matchPassword(password)) {
+		res.status(200).json({
 			_id: user._id,
 			name: user.name,
 			email: user.email,
--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,8 +1,12 @@
 import express from "express";
-import { createUser } from "../controllers/user.controller.js";
+import {
+	authenticateUser,
+	createUser,
+} from "../controllers/user.controller.js";
 
 const router = express.Router();
 
 router.route("/").post(createUser);
+router.post("/login", authenticateUser);
 
 export default router;
