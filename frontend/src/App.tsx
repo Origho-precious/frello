e66478129978a75fcd6e4ad53d70afc9a091d7db
Origho-precious <origho9@gmail.com>
2021-07-15 12:29:33 +0100

    feat: securing routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,21 @@
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { selectAuthState } from "./store/slices/auth.slice";
 import Home from "./pages/home";
+import Boards from "./pages/boards";
 
 const App = () => {
+	const { authenticated } = useSelector(selectAuthState);
+
 	return (
 		<BrowserRouter>
 			<Switch>
-				<Route exact path="/" component={Home} />
+				<Route exact path="/">
+					{authenticated ? <Redirect to="/board" /> : <Home />}
+				</Route>
+				<Route exact path="/board">
+					{!authenticated ? <Redirect to="/" /> : <Boards />}
+				</Route>
 			</Switch>
 		</BrowserRouter>
 	);
