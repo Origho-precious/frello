6692c542a6964c78d85f1c10a2267e25e0d50e07
Origho-precious <origho9@gmail.com>
2021-07-21 05:27:55 +0100

    feat: fetching boards from BE

--- frontend/src/pages/boards/index.tsx
+++ frontend/src/pages/boards/index.tsx
@@ -1,31 +1,34 @@
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 import styled from "styled-components";
 import BoardCard from "../../components/composed/Cards/BoardCard/BoardCard";
 import Navbar from "../../components/composed/Navbar/Navbar";
-
-const boards = [
-	{
-		id: "hhthey5994",
-		title: "Test board",
-	},
-	{
-		id: "ggtheydd5654",
-		title: "Learning Nodejs with typescript",
-	},
-	{
-		id: "hhthey43254",
-		title: "learning Golang and preparing for google",
-	},
-	{
-		id: "hrtheujn5654",
-		title: "learning Golang and preparing for google",
-	},
-	{
-		id: "rhthey88854",
-		title: "learning Golang and preparing for google",
-	},
-];
+import { IBoard } from "../../interfaces/IBoard.interface";
+import { selectAuthState } from "../../store/slices/auth.slice";
+import { APIClient } from "../../utils/apiClient";
 
 const Boards = () => {
+	const {
+		profile: { token },
+	} = useSelector(selectAuthState);
+	const [boards, setBoards] = useState<IBoard[]>([]);
+
+	
+	useEffect(() => {
+		const getUserCreatedBoards = async () => {
+			const api = new APIClient();
+	
+			try {
+				const res = await api.getUserCreatedBoards(token as string);
+				setBoards(res)
+			} catch (error) {
+				console.log(error);
+			}
+		};
+		
+		getUserCreatedBoards();
+	}, [token]);
+
 	return (
 		<Wrapper>
 			<Navbar />
@@ -33,8 +36,8 @@ const Boards = () => {
 				<h2 className="text-3xl font-bold mb-4">BOARDS</h2>
 				<section className="flex justify-center lg:justify-start flex-wrap sm:-ml-2">
 					{boards.map((board) => (
-						<div key={board?.id} className="mx-2 mb-4">
-							<BoardCard id={board?.id} title={board?.title} />
+						<div key={board?._id} className="mx-2 mb-4">
+							<BoardCard id={board?._id} title={board?.title} />
 						</div>
 					))}
 				</section>
@@ -43,8 +46,8 @@ const Boards = () => {
 	);
 };
 const Wrapper = styled.div`
-	& > article{
-		@media screen and (max-width: 500px){
+	& > article {
+		@media screen and (max-width: 500px) {
 			max-width: 95%;
 		}
 	}
