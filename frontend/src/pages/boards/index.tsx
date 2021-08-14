97fcbf9ce3864835316cf1fe30f0967c9eba2a9d
Origho-precious <origho9@gmail.com>
2021-08-14 20:47:51 +0100

    code refactor

--- frontend/src/pages/boards/index.tsx
+++ frontend/src/pages/boards/index.tsx
@@ -3,6 +3,7 @@ import { useSelector } from "react-redux";
 import styled from "styled-components";
 import BoardCard from "../../components/composed/Cards/BoardCard/BoardCard";
 import Navbar from "../../components/composed/Navbar/Navbar";
+import Spinner from "../../components/composed/Spinner/Spinner";
 import { IBoard } from "../../interfaces/IBoard.interface";
 import { selectAuthState } from "../../store/slices/auth.slice";
 import { APIClient } from "../../utils/apiClient";
@@ -11,23 +12,44 @@ const Boards = () => {
 	const {
 		profile: { token },
 	} = useSelector(selectAuthState);
-	const [boards, setBoards] = useState<IBoard[]>([]);
+	const [createdBoards, setCreatedBoards] = useState<IBoard[]>([]);
+	const [boardInvitedTo, setBoardsInvitedto] = useState<IBoard[]>([]);
+	const [loading, setLoading] = useState(false);
 
-	
+	const getUserCreatedBoards = async () => {
+		const api = new APIClient();
+
+		try {
+			setLoading(true)
+			const res = await api.getUserCreatedBoards(token as string);
+			setCreatedBoards(res);
+			setLoading(false);
+		} catch (error) {
+			setLoading(false);
+			console.log(error);
+		}
+	};
+
+	const getBoardsInvitedto = async () => {
+		const api = new APIClient();
+
+		try {
+			const res = await api.getUserCreatedBoards(token as string);
+			setBoardsInvitedto(res);
+		} catch (error) {
+			setLoading(false);
+			console.log(error);
+		}
+	};
+
+	const fetchAll = async () => {
+		await Promise.all([getUserCreatedBoards(), getBoardsInvitedto()]);
+	};
+
 	useEffect(() => {
-		const getUserCreatedBoards = async () => {
-			const api = new APIClient();
-	
-			try {
-				const res = await api.getUserCreatedBoards(token as string);
-				setBoards(res)
-			} catch (error) {
-				console.log(error);
-			}
-		};
-		
-		getUserCreatedBoards();
-	}, [token]);
+		fetchAll();
+	// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, []);
 
 	return (
 		<Wrapper>
@@ -35,7 +57,8 @@ const Boards = () => {
 			<article className="w-11/12 md:w-9/12 mx-auto mt-6">
 				<h2 className="text-3xl font-bold mb-4">BOARDS</h2>
 				<section className="flex justify-center lg:justify-start flex-wrap sm:-ml-2">
-					{boards.map((board) => (
+					{true && <Spinner size={200} />}
+					{createdBoards?.map((board) => (
 						<div key={board?._id} className="mx-2 mb-4">
 							<BoardCard id={board?._id} title={board?.title} />
 						</div>
