cc50d4f0fc60267033538dcb49f76088beadd75b
Origho-precious <Origho9@gmail.com>
2021-09-17 23:43:43 +0100

    fix: refactoring board controller

--- backend/controllers/board.controller.js
+++ backend/controllers/board.controller.js
@@ -2,26 +2,50 @@ import Board from "../models/board.model.js";
 import asyncHandler from "express-async-handler";
 
 export const createBoard = asyncHandler(async (req, res) => {
-	const userId = req.user._id;
-	const { title } = req.body;
+	try {
+		const userId = req.user._id;
+		const { title } = req.body;
 
-	if (!title) {
-		res.status(400);
-		throw new Error("Board title not found!");
-	} else {
-		await Board.create({ title, createdBy: userId });
-		res.status(201).json('Board created successfully!');
+		if (!title) {
+			res.status(400);
+			throw new Error("Board title not found!");
+		} else {
+			await Board.create({ title, createdBy: userId });
+			res.status(201).json("Board created successfully!");
+		}
+	} catch (error) {
+		console.error(error.message);
+		res.status(500);
+		throw new Error("Server Error, Something went wrong!");
 	}
 });
 
 export const getUserBoards = asyncHandler(async (req, res) => {
-	const createdBy = req.user._id;
-	const boards = await Board.find({ createdBy }).populate("createdBy members", "id name email");
-	res.status(200).json(boards);
+	try {
+		const createdBy = req.user._id;
+		const boards = await Board.find({ createdBy }).populate(
+			"createdBy members",
+			"id name email"
+		);
+		res.status(200).json(boards);
+	} catch (error) {
+		console.error(error.message);
+		res.status(500);
+		throw new Error("Server Error, Something went wrong!");
+	}
 });
 
 export const getBoardsInvitedTo = asyncHandler(async (req, res) => {
-	const userId = req.user._id;
-	const boards = await Board.find({ members: userId }).populate("createdBy members", "id name email");
-	res.status(200).json(boards);
+	try {
+		const userId = req.user._id;
+		const boards = await Board.find({ members: userId }).populate(
+			"createdBy members",
+			"id name email"
+		);
+		res.status(200).json(boards);
+	} catch (error) {
+		console.error(error.message);
+		res.status(500);
+		throw new Error("Server Error, Something went wrong!");
+	}
 });
